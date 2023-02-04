10f51e3badec4b302ee56e9246bfecd03f456607
Alef Zain Gama Sandoval <alfa_gama_32@hotmail.com>
2023-02-03 19:51:50 -0600

    Clase 4. Repaso 2

--- Sesion-04/Reto-02/codigo/src/org/bedu/java/jse/basico/sesion4/reto2/CombustibleAlef.java
+++ Sesion-04/Reto-02/codigo/src/org/bedu/java/jse/basico/sesion4/reto2/CombustibleAlef.java
@@ -1,5 +1,72 @@
 package org.bedu.java.jse.basico.sesion4.reto2;
 
 public class CombustibleAlef {
+    private short litrosMax;
+    private short litrosAhora;
+    public CombustibleAlef(short litrosMax, short litrosAhora) {
+        this.litrosMax = litrosMax;
+        this.litrosAhora = litrosAhora;
+    }
     
+    // Método
+    public void restarLitros() {
+        litrosAhora--;
+        System.out.println("Quedan " + litrosAhora + " litros.");
+    }
+
+    /**
+     * @return short return the litrosMax
+     */
+    public short getlitrosMax() {
+        return litrosMax;
+    }
+
+    /**
+     * @param litrosMax the litrosMax to set
+     */
+    public void setlitrosMax(short litrosMax) {
+        this.litrosMax = litrosMax;
+    }
+
+    /**
+     * @return short return the litrosAhora
+     */
+    public short getlitrosAhora() {
+        return litrosAhora;
+    }
+
+    /**
+     * @param litrosAhora the litrosAhora to set
+     */
+    public void setlitrosAhora(short litrosAhora) {
+        this.litrosAhora = litrosAhora;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + litrosMax;
+        result = prime * result + litrosAhora;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CombustibleAlef other = (CombustibleAlef) obj;
+        if (litrosMax != other.litrosMax)
+            return false;
+        if (litrosAhora != other.litrosAhora)
+            return false;
+        return true;
+    }
+
+    
+
 }
--- Sesion-04/Reto-02/codigo/src/org/bedu/java/jse/basico/sesion4/reto2/LlantaAlef.java
+++ Sesion-04/Reto-02/codigo/src/org/bedu/java/jse/basico/sesion4/reto2/LlantaAlef.java
@@ -10,7 +10,7 @@ public class LlantaAlef {
         this.presionAlef = presionAlef;
     }
 
-    // Rodar
+    // // Rodar
     // public ruedaAlef () {
     //     System.out.println("Rodando ando :)");
     // }
@@ -34,7 +34,30 @@ public class LlantaAlef {
     public float getPresionAlef() {
         return presionAlef;
     }
+
     public void setPresionAlef(float presionAlef) {
         this.presionAlef = presionAlef;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        LlantaAlef llantaAlef = (LlantaAlef) o;
+        if (Float.compare(llantaAlef.anchoAlef, anchoAlef) != 0)
+            return false;
+        if (Float.compare(llantaAlef.diametroAlef, diametroAlef) != 0)
+            return false;
+        return Float.compare(llantaAlef.presionAlef, presionAlef) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int resultAlef = (anchoAlef != +0.0F ? Float.floatToIntBits(anchoAlef) : 0);
+        resultAlef = 31 * resultAlef + (diametroAlef != +0.0f ? Float.floatToIntBits(diametroAlef) : 0);
+        resultAlef = 31 * resultAlef + (presionAlef != +0.0f ? Float.floatToIntBits(presionAlef) : 0);
+        return resultAlef;
     }
 }
